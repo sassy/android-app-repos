d02d49fd3911889464d5ffa5a206bc3d9af2b95d
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-02-11 16:02:36 +0900

    implement Affirmations app

--- Affirmations/app/src/main/java/io/github/sassy/affirmations/MainActivity.kt
+++ Affirmations/app/src/main/java/io/github/sassy/affirmations/MainActivity.kt
@@ -2,14 +2,17 @@ package io.github.sassy.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.github.sassy.affirmations.adapter.ItemAdapter
 import io.github.sassy.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textview = findViewById<TextView>(R.id.textview)
-        textview.text = Datasource().loadAffirmations().size.toString()
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
