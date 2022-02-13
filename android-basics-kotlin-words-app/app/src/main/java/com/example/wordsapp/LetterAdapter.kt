27e970f347ff99ba900cfab3c3be73328e753a07
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-02-14 00:28:25 +0900

    use navGraph for words app.

--- android-basics-kotlin-words-app/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ android-basics-kotlin-words-app/app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -23,8 +23,9 @@ import android.view.ViewGroup
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Button
 import androidx.annotation.RequiresApi
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.example.wordsapp.DetailActivity.Companion.LETTER
+import com.example.wordsapp.WordListFragment.Companion.LETTER
 
 /**
  * Adapter for the [RecyclerView] in [MainActivity].
@@ -65,10 +66,11 @@ class LetterAdapter :
         val item = list.get(position)
         holder.button.text = item.toString()
         holder.button.setOnClickListener {
-            val context = holder.view.context
-            val intent = Intent(context, DetailActivity::class.java)
-            intent.putExtra(LETTER, holder.button.text.toString())
-            context.startActivity(intent)
+            val action = LetterListFragmentDirections
+                .actionLetterListFragmentToWordListFragment(
+                    letter = holder.button.text.toString()
+                )
+            holder.view.findNavController().navigate(action)
         }
     }
 
